51aab0089019ecabe066afe381a7e0e0e06debba
tnk050 <hexangleff@gmail.com>
2022-03-19 11:15:03 +0900

    in dev

--- src/main.ts
+++ src/main.ts
@@ -9,33 +9,35 @@ function doGet(): GoogleAppsScript.HTML.HtmlOutput {
   return HtmlService.createHtmlOutputFromFile("index");
 }
 
-function processForm(formObject: { myFile }): Blob {
+function processForm(formObject: { myFile }): GoogleAppsScript.Base.Blob {
   const formBlob = formObject.myFile;
   const csvStr = formBlob.getDataAsString();
-  const csv = Utilities.parseCsv(csvStr);
-  // csvを編集する
-  // 編集したcsvをかえす。←htmlフォーム？
-  return;
+  const request = Utilities.parseCsv(csvStr);
+  const editData = editQue(request);
+  const response = createCsv("convertedque", editData);
+  return response; //returnするだけじゃダメ
 }
 
-function editCsv(csv: string[][]): string[][] {
+function editQue(csv: string[][]): string[][] {
   const response = [
     ["No.", "ポイント", "方角", "道路", "合計", "備考", "説明"],
   ];
+  const typeName = "Type";
+  const notesName = "Notes";
+  const distName = "Distance (km) From Start";
+  const descriptionName = "Description";
   const title = csv.shift();
-  const typeClm = title.indexOf("Type");
-  const notesClm = title.indexOf("Notes");
-  const distClm = title.indexOf("Distance (km) From Start");
-  const descriptionClm = title.indexOf("Description");
+  const typeClm = title.indexOf(typeName);
+  const notesClm = title.indexOf(notesName);
+  const distClm = title.indexOf(distName);
+  const descriptionClm = title.indexOf(descriptionName);
   [
-    { Type: typeClm },
-    { Notes: notesClm },
-    { Distance: distClm },
-    { Description: descriptionClm },
+    [typeName, typeClm],
+    [notesName, notesClm],
+    [distName, distClm],
+    [descriptionName, descriptionClm],
   ].forEach(
-    (item) =>
-      Object.values(item)[0] === -1 &&
-      response.push(addError(Object.keys(item)[0]))
+    (item) => item[1] === -1 && response.push([addError(item[0] as string)])
   );
   for (let i = 1; i < csv.length; i++) {
     const type = csv[i][typeClm];
@@ -54,8 +56,23 @@ function editCsv(csv: string[][]): string[][] {
     response.push(addData);
   }
   return response;
+
+  function addError(input: string): string {
+    return `エラー: ${input}列が存在しません`;
+  }
 }
 
-function addError(input: string): string {
-  return `エラー: ${input}列が存在しません`;
+function createCsv(
+  name: string,
+  data: string[][],
+  charset: string = "utf-8"
+): GoogleAppsScript.Base.Blob {
+  const fileName = name + ".csv";
+  const contentType = "text/csv";
+  const dataStr = data.map((item) => item.join(",")).join("\r\n");
+  const blob = Utilities.newBlob("", contentType, fileName).setDataFromString(
+    dataStr,
+    charset
+  );
+  return blob;
 }
